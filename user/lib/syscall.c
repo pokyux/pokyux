8005481b21fa62ff8bea9a2ac9b5d99f4bd7acd8
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-28 15:41:33 +0800

    add user syscall

---
+++ user/lib/syscall.c
@@ -0,0 +1,20 @@
+#include "syscall.h"
+
+isize syscall(
+  usize id, 
+  usize arg0, usize arg1, usize arg2
+) {
+  usize result;
+  asm volatile (
+    "mv a0, %2\n" // arg0
+    "mv a1, %3\n" // arg1
+    "mv a2, %4\n" // arg2
+    "mv a7, %1\n" // which
+    "ecall\n"
+    "mv %0, a0\n" // ret
+    : "=r"(result)
+    : "r"(id), "r"(arg0), "r"(arg1), "r"(arg2)
+    : "memory"
+  );
+  return result;
+}
---
+++ user/lib/syscall.h
@@ -0,0 +1,6 @@
+#include "type.h"
+
+isize syscall(
+  usize id, 
+  usize arg0, usize arg1, usize arg2
+);
