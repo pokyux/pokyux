c44e5c6d2e4ca4f6d40e889cb0ea76f3275266c4
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-28 15:37:54 +0800

    copy type.h to user lib

---
+++ user/lib/type.h
@@ -0,0 +1,23 @@
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned long
+#define u64 unsigned long long
+
+#define i8 char
+#define i16 short
+#define i32 long
+#define i64 long long
+
+#ifdef PKX32
+#define usize long
+#define isize long
+#else
+#define usize long long
+#define isize long long
+#endif
+
+#define true 1
+#define false 0
+#define bool u8
+
+#define nullptr 0
