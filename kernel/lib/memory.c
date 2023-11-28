4673b8c70565364352c3f58f5e568b83355a0cc7
Pokyux <pokyux@mail.ustc.edu.cn>
2023-11-28 19:11:28 +0800

    pkx_memcpy

--- kernel/lib/memory.c
+++ kernel/lib/memory.c
@@ -6,3 +6,8 @@ bool pkx_memcmp(u8 *a, u8 *b, usize len) {
       return false;
   return true;
 }
+
+bool pkx_memcpy(u8 *dst, u8 *src, usize len) {
+  for (usize i = 0; i < len; i++)
+    dst[i] = src[i];
+}
--- kernel/lib/memory.h
+++ kernel/lib/memory.h
@@ -1,1 +1,4 @@
+#include "type.h"
+
 bool pkx_memcmp(u8 *a, u8 *b, usize len);
+bool pkx_memcpy(u8 *a, u8 *b, usize len);
