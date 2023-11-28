839d7eac5c3eb9f582340ba305a7fc07d411bbd2
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-28 17:20:22 +0800

    show hello app addr

--- kernel/init/main.c
+++ kernel/init/main.c
@@ -3,6 +3,9 @@
 #include "stdio.h"
 
 usize pkx_start() {
-  pkx_printk("%s %d\n", "Pokyux kernel started.", 233);
+  extern void pkx_entry;
+  pkx_printk("Pokyux kernel addr: %x\n", &pkx_entry);
+  extern void app_hello_start;
+  pkx_printk("App hello addr: %x\n", &app_hello_start);
   while (true) pkx_idle();
 }
