c0e51a35a6a9543cce4bf9eb12d60757531549fa
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-29 14:16:00 +0800

    add syscall

---
+++ kernel/syscall/syscall.c
@@ -0,0 +1,8 @@
+#include "syscall.h"
+
+usize pkx_syscall(
+  usize id, 
+  usize arg0, usize arg1, usize arg2
+) {
+  
+}
---
+++ kernel/syscall/syscall.h
@@ -0,0 +1,6 @@
+#include "type.h"
+
+usize pkx_syscall(
+  usize id, 
+  usize arg0, usize arg1, usize arg2
+);
