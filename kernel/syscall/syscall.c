cec4a200242f9025e9bcaa9b403a82e8f81c1722
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-29 14:25:18 +0800

    wrap syscalls

--- kernel/syscall/syscall.c
+++ kernel/syscall/syscall.c
@@ -1,8 +1,19 @@
 #include "syscall.h"
+#include "io.h"
+#include "stdio.h"
 
 usize pkx_syscall(
   usize id, 
   usize arg0, usize arg1, usize arg2
 ) {
-  
+  switch (id)
+  {
+  case PKX_SYS_WRITE:
+    return pkx_sys_write(arg0, (char*) arg1, arg2);
+  case PKX_SYS_EXIT:
+    return pkx_sys_exit(arg0);
+  default:
+    pkx_printk("Error: Unknown syscall id: %d\n", id);
+    return -1;
+  }
 }
--- kernel/syscall/syscall.h
+++ kernel/syscall/syscall.h
@@ -1,5 +1,8 @@
 #include "type.h"
 
+#define PKX_SYSCALL_WRITE 64
+#define PKX_SYSCALL_EXIT  93
+
 usize pkx_syscall(
   usize id, 
   usize arg0, usize arg1, usize arg2
