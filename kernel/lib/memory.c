953e6d89019238e76d7375e552823245ed02d1f6
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-29 23:13:27 +0800

    pkx_memset

--- kernel/lib/memory.c
+++ kernel/lib/memory.c
@@ -15,3 +15,8 @@ bool pkx_memcpy(u8 *dst, const u8 *src, usize len) {
 void memcpy(void *dst, const void *src, usize len) {
   pkx_memcpy((u8*) dst, (const u8*) src, len);
 }
+
+void pkx_memset(u8 *a, u8 val, usize len) {
+  for (usize i = 0; i < len; i++)
+    a[i] = val;
+}
--- kernel/lib/memory.h
+++ kernel/lib/memory.h
@@ -3,3 +3,4 @@
 bool pkx_memcmp(u8 *a, u8 *b, usize len);
 bool pkx_memcpy(u8 *a, u8 *b, usize len);
 void memcpy(void *dst, const void *src, usize len);
+void pkx_memset(u8 *a, u8 val, usize len);
