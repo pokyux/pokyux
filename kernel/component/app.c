4390ee7fc21d0f596cd1dad8827a955d7ad8dfe5
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-29 17:28:25 +0800

    remove init kernel stack

--- kernel/component/app.c
+++ kernel/component/app.c
@@ -2,11 +2,6 @@
 #include "pkx_call.h"
 #include "stdio.h"
 
-void pkx_init_kernel_stack() {
-  pkx_kernel_sp = (u8*) (((usize) pkx_kernel_stack) + PKX_KERNEL_STACK_SIZE);
-  pkx_printk("Kernel stack init ok.\n");
-}
-
 bool pkx_load_app_bin(usize addr, usize len) {
   pkx_printk("Loading app from addr: 0x%x, size: 0d%d\n", addr, len);
   pkx_memcpy((u8*) 0x80400000, (u8*) addr, len);
--- kernel/component/app.h
+++ kernel/component/app.h
@@ -15,7 +15,6 @@ static u8 pkx_user_stack[PKX_USER_STACK_SIZE];
 static u8 pkx_kernel_stack[PKX_KERNEL_STACK_SIZE];
 #endif
 
-void pkx_init_kernel_stack();
 bool pkx_load_app_bin(usize addr, usize len);
 void pkx_next_app();
 void pkx_push_stack(u8 *sp, u8 *content, usize len);
