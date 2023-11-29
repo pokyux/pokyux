44f81ace72a540e58657379a7ff26cd558221e38
pokyux <pokyux@mail.ustc.edu.cn>
2023-11-29 23:15:50 +0800

    fix pkx_memcpy def

--- kernel/lib/memory.h
+++ kernel/lib/memory.h
@@ -1,6 +1,6 @@
 #include "type.h"
 
 bool pkx_memcmp(u8 *a, u8 *b, usize len);
-bool pkx_memcpy(u8 *a, u8 *b, usize len);
+bool pkx_memcpy(u8 *dst, const u8 *src, usize len);
 void memcpy(void *dst, const void *src, usize len);
 void pkx_memset(u8 *a, u8 val, usize len);
