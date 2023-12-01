ed509a80edf8e8aab115459f006f4d7b7751683b
pokyux <pokyux@mail.ustc.edu.cn>
2023-12-01 15:22:35 +0800

    init mem

--- kernel/component/mem.c
+++ kernel/component/mem.c
@@ -4,9 +4,14 @@
 
 void *pkx_alloc_top_addr;
 
+void pkx_init_mem() {
+  pkx_alloc_top_addr = PKX_ALLOC_BASE_ADDR;
+}
+
 void *pkx_alloc(usize size) {
   void *ret = pkx_alloc_top_addr;
   pkx_alloc_top_addr += size;
+  return ret;
 }
 
 void *pkx_free(void *addr) {
--- kernel/component/mem.h
+++ kernel/component/mem.h
@@ -1,4 +1,5 @@
 #include "type.h"
 
+void pkx_init_mem();
 void *pkx_alloc(usize size);
 void *pkx_free(void *addr);
