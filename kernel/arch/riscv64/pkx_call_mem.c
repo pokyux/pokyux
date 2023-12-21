2962ce8aa1b1bab2be6c5f9ce58efd9a2df3e14e
Pokyux <pokyux@mail.ustc.edu.cn>
2023-12-21 11:58:52 +0800

    define pkx ppn manager

--- kernel/arch/riscv64/pkx_call_mem.c
+++ kernel/arch/riscv64/pkx_call_mem.c
@@ -1,6 +1,13 @@
 #define PKX_PHYS_MEM_START 0x80200000
 #define PKX_PHYS_MEM_END   0x80800000 // not included
 
+struct _pkx_ppn_manager {
+  usize start, end;         // end is not included
+  u8 allocation_table[192]; // caculate all avail phys mem
+} pkx_ppn_manager;
+
 void pkx_init_mem(u8 *kernel_start, u8 *kernel_end);
 void *pkx_alloc(usize size);
 void pkx_free(void *addr);
+usize pkx_alloc_ppn();
+void pkx_free_ppn();
