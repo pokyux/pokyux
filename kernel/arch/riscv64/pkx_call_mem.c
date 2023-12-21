dd6369ef4d111fb9b52b3f5460571916e7c6b117
Pokyux <pokyux@mail.ustc.edu.cn>
2023-12-21 11:29:05 +0800

    define K210 addr range

--- kernel/arch/riscv64/pkx_call_mem.c
+++ kernel/arch/riscv64/pkx_call_mem.c
@@ -1,3 +1,6 @@
+#define PKX_PHYS_MEM_START 0x80200000
+#define PKX_PHYS_MEM_END   0x80800000 // not included
+
 void pkx_init_mem(u8 *kernel_start, u8 *kernel_end);
 void *pkx_alloc(usize size);
 void pkx_free(void *addr);
