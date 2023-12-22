964243c0691bce490663847938ebba830adff018
pokyux <pokyux@mail.ustc.edu.cn>
2023-12-22 17:32:12 +0800

    add def: get addr ppn

--- kernel/arch/riscv64/pkx_call_mem.c
+++ kernel/arch/riscv64/pkx_call_mem.c
@@ -1,6 +1,10 @@
 #define PKX_PHYS_MEM_START 0x80200000
 #define PKX_PHYS_MEM_END   0x80800000 // not included
 
+// pokyux 在 RISCV64 上使用 SV39 地址结构
+// SV39: Phys: 44 PPN + 12 offset
+//       Virt: 27 VPN + 12 offset
+
 struct _pkx_ppn_manager {
   usize start, end;         // end is not included
   u8 allocation_table[192]; // caculate all avail phys mem
@@ -9,4 +13,5 @@ struct _pkx_ppn_manager {
 void pkx_init_mem(u8 *kernel_start, u8 *kernel_end);
 usize pkx_alloc_ppn();
 void pkx_free_ppn();
+usize pkx_get_addr_ppn(void *_addr);
 void *pkx_get_pp_addr(usize ppn);
