022c248f171896a614c1943523b9d274bb6f1b59
pokyux <pokyux@mail.ustc.edu.cn>
2023-12-22 18:20:55 +0800

    impl: ppn allocation free

--- kernel/arch/riscv64/pkx_call_mem.c
+++ kernel/arch/riscv64/pkx_call_mem.c
@@ -7,7 +7,7 @@
 //       Virt: 27 VPN + 12 offset
 
 struct _pkx_ppn_manager {
-  usize start, usable;      // start: 第一个物理页号, usable: 可用页面总数
+  usize start, usable, allocation_table_lenth;      // start: 第一个物理页号, usable: 可用页面总数
   u8 allocation_table[192]; // bit map. 1 for avail, 0 for used.
 } pkx_ppn_manager;
 
@@ -19,6 +19,7 @@ void pkx_init_mem(u8 *kernel_start, u8 *kernel_end) {
   // 目前 Pokyux 使用位图管理内存页，所以要求页面总数是 8 的倍数
   pkx_ppn_manager.usable -= pkx_ppn_manager % 8;
   usize bitmap_lenth = pkx_ppn_manager.usable / 8;
+  pkx_ppn_manager.allocation_table_lenth = bitmap_lenth;
 
   if (bitmap_lenth > 192)
     pkx_panic("Phys mem too large.");
@@ -32,8 +33,42 @@ void pkx_init_mem(u8 *kernel_start, u8 *kernel_end) {
   pkx_printk("Start PPN: 0x%x, Total PPN: d%d\n", start, pkx_ppn_manager.usable);
 }
 
-usize pkx_alloc_ppn();
-void pkx_free_ppn();
+u8 pkx_bit_selector[8] = {
+  1 << 0,
+  1 << 1,
+  1 << 2,
+  1 << 3,
+  1 << 4,
+  1 << 5,
+  1 << 6,
+  1 << 7
+};
+
+usize pkx_alloc_ppn() {
+  usize i = 0;
+  for (; i < pkx_ppn_manager.allocation_table_lenth; i++)
+    if (pkx_ppn_manager.allocation_table[i])
+      break;
+  if (i == pkx_ppn_manager.allocation_table_lenth)
+    return 0;
+  for (usize j = 0; j < 8; j++) {
+    if (pkx_ppn_manager.allocation_table[i] & pkx_bit_selector[j]) {
+      pkx_ppn_manager.allocation_table[i] &= ~(pkx_bit_selector[j]);
+      return pkx_ppn_manager.start + i * 8 + j;
+    }
+  }
+  return 0;
+}
+
+void pkx_free_ppn(usize ppn) {
+  if (ppn < pkx_ppn_manager.start)
+    return;
+  usize bitmap_location = (ppn - pkx_ppn_manager.start) / 8;
+  usize in_bit_offset   = (ppn - pkx_ppn_manager.start) % 8;
+  if (bitmap_location >= pkx_ppn_manager.allocation_table_lenth)
+    return;
+  pkx_ppn_manager.allocation_table[bitmap_location] |= pkx_bit_selector[in_bit_offset];
+}
 
 usize pkx_addr_to_ppn(void *_addr) {
   usize addr = (usize) _addr;
