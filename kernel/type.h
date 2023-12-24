936f3c50ceab55b4df8df4e791439147ab0b8b2e
pokyux <pokyux@mail.ustc.edu.cn>
2023-12-24 22:22:07 +0800

    fix usize isize

--- kernel/type.h
+++ kernel/type.h
@@ -1,18 +1,18 @@
-#define u8 unsigned char
+#define u8  unsigned char
 #define u16 unsigned short
 #define u32 unsigned long
 #define u64 unsigned long long
 
-#define i8 char
+#define i8  char
 #define i16 short
 #define i32 long
 #define i64 long long
 
 #ifdef PKX32
-#define usize long
+#define usize unsigned long
 #define isize long
 #else
-#define usize long long
+#define usize unsigned long long
 #define isize long long
 #endif
 
